251e4ba8b105e4178db78e449fa7501a4fece75b
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-04-26 10:34:13 +0300

    added page route structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/exchange/stock">Stock</a>
+<br>
+<a href="/tax/inheritance">Inheritance tax</a>
+<br>
+<p>Home</p>
---
+++ src/routes/tax/inheritance/+page.svelte
@@ -0,0 +1,1 @@
+<p>tax / inheritance</p>
